e76aa705c08d4fd3734bbdd6cef2a05cb248733a
honglinchenoscar <hchen09@risd.edu>
2024-08-08 15:38:11 -0400

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Button } from 'antd';
 
 const App = () => (
+  <>
   <div className="App">
     <Button type="primary">Button</Button>
   </div>
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+  </>
 );
 
 export default App;
