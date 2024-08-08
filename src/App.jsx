2c4bb7d7172c632440a1c0780763d60387d3ccf8
honglinchenoscar <hchen09@risd.edu>
2024-08-08 16:16:27 -0400

    Add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,74 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Form, Input, Select } from 'antd';
 
-const App = () => (
-  <>
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-  </>
-);
+const { Option } = Select;
+
+const App = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <Form
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      style={{ maxWidth: 600 }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="nickname"
+        label="Nickname"
+        tooltip="What do you want others to call you?"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your nickname!',
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="gender"
+        label="Gender"
+        rules={[
+          {
+            required: true,
+            message: 'Please select gender!',
+          },
+        ]}
+      >
+        <Select placeholder="select your gender">
+          <Option value="male">Male</Option>
+          <Option value="female">Female</Option>
+          <Option value="other">Other</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        name="intro"
+        label="Intro"
+        rules={[
+          {
+            required: true,
+            message: 'Please input Intro',
+          },
+        ]}
+      >
+        <Input.TextArea showCount maxLength={100} />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default App;
